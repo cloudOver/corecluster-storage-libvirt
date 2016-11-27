a8f063de02ffd26e3cc658ae19b3f11fb6eec71a
Maciej Nabozny <maciej.nabozny@cloudover.io>
2016-11-27 13:11:42 +0100

    setup.py

---
+++ setup.py
@@ -0,0 +1,22 @@
+from distutils.core import setup
+from setuptools import find_packages
+from distutils.command.install import install as _install
+
+def version():
+    f = open('debian/changelog')
+    v = f.readline().split()[1]
+    return v[1:-1]
+
+setup(
+  name = 'corecluster-storage-libvirt',
+  packages = find_packages(exclude=['config', 'config.*']),
+  version = '16.11.01',
+  description = 'Libvirt storage backend for CoreCluster',
+  author = 'Maciej Nabozny',
+  author_email = 'maciej.nabozny@cloudover.io',
+  url = 'http://cloudover.org/corecluster/',
+  download_url = 'https://github.com/cloudOver/CoreCluster/archive/master.zip',
+  keywords = ['corecluster'],
+  classifiers = [],
+  install_requires = ['corecluster'],
+)
