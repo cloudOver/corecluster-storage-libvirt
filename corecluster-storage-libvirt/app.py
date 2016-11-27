33879581488599b9b31c9035ee76b47d1fccb4e7
Maciej Nabozny <maciej.nabozny@cloudover.io>
2016-11-27 11:44:30 +0100

    Fixed typos

--- corecluster-storage-libvirt/app.py
+++ corecluster-storage-libvirt/app.py
@@ -1,8 +1,8 @@
 MODULE = {
     'agents': [
-        {'type': 'image', 'module': 'corecluster.agents.image_libvirt', 'count': 4},
-        {'type': 'node', 'module': 'corecluster.agents.node_libvirt', 'count': 4},
-        {'type': 'storage', 'module': 'corecluster.agents.storage_libvirt', 'count': 4},
+        {'type': 'image', 'module': 'corecluster-storage-libvirt.agents.image_libvirt', 'count': 4},
+        {'type': 'node', 'module': 'corecluster-storage-libvirt.agents.node_libvirt', 'count': 4},
+        {'type': 'storage', 'module': 'corecluster-storage-libvirt.agents.storage_libvirt', 'count': 4},
     ],
     'drivers': {
         
